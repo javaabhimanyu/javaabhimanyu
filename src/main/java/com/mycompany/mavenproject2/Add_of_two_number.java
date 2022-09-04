54454be40c2d8d5bde813e610a7546186655d0f6
Abhimanyu <javaabhimanyu@gmail.com>
2022-09-04 22:15:29 +0530

    Two Number add

---
+++ src/main/java/com/mycompany/mavenproject2/Add_of_two_number.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Abhimanyu Singh
+ */
+public class Add_of_two_number {
+    public static void main(String []arg){
+        int a,b,c;
+        System.out.print("Enter the number: ");
+        Scanner s=new Scanner(System.in);
+      a=s.nextInt();
+         b=s.nextInt();
+         
+          c=a+b;
+        System.out.println("Totalsum of two number: "+c);
+      
+    }
+    
+}
