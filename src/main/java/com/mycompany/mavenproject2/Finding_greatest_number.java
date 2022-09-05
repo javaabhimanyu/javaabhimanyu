7d4e6d6c7a0d61bc5fcd0e841daff3722d555830
Abhimanyu <javaabhimanyu@gmail.com>
2022-09-05 12:41:40 +0530

    Compleated Greatest number

---
+++ src/main/java/com/mycompany/mavenproject2/Finding_greatest_number.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author Abhimanyu Singh
+ */
+public class Finding_greatest_number {
+    public static void main(String[]arg){
+        int a=10,b=20,c=7;
+        int r=(a>b)?(a>c?a:c):(b>c?b:c);
+        System.out.print(r);
+    }
+    
+}
