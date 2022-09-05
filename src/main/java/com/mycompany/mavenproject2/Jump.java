d85b15367cea2e6a0647ff024ae241e4b0ab949a
Abhimanyu <javaabhimanyu@gmail.com>
2022-09-05 17:46:26 +0530

    Done Jump code

---
+++ src/main/java/com/mycompany/mavenproject2/Jump.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author Abhimanyu Singh
+ */
+public class Jump {
+    public static void main(String []args){
+        for(int i=1;i<=10;i++)
+        {
+            if(i==5)  
+            continue;
+            System.out.print(i+" ");
+        }
+    }
+}
